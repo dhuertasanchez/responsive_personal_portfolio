e06d38763f0a6d44a34ace9672bc3bea3761b98f
Daniel Huerta <dhuertasanchez@gmail.com>
2021-08-30 19:20:21 -0500

    Se detuvo el desarrollo en la sección de contacto.

--- js/main.js
+++ js/main.js
@@ -72,7 +72,7 @@ tabs.forEach(tab => {
 /*==================== SERVICES MODAL ====================*/
 const modalViews = document.querySelectorAll('.services__modal'),
       modalBtns = document.querySelectorAll('.services__button'),
-      modalCloses = document.querySelectorAll('services__modal-close')
+      modalCloses = document.querySelectorAll('.services__modal-close')
 
 let modal = function(modalClick) {
     modalViews[modalClick].classList.add('active-modal')
@@ -91,3 +91,36 @@ modalCloses.forEach((modalClose) => {
         })
     })
 })
+
+/*==================== PORTFOLIO SWIPPER ====================*/
+let swiperPortfolio = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+/*==================== TESTIMONIAL SWIPPER ====================*/
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        568: {
+            slidesPerView: 2,
+        }
+    }
+});
